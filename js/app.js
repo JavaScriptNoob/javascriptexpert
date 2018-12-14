53547ca451e74f632c9199fc2233e98ef4a3d782
Balabol <grustniiii@gmail.com>
2018-12-14 17:27:14 +0100

    refaktoring

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 var myApp = angular.module('myApp', ['firebase', 'ngRoute']);
-
-
 myApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
@@ -12,60 +10,133 @@ myApp.config(function ($routeProvider) {
             controller: "loginCtrl"
         });
 });
+var users_list = [];
+var users_coords = {};
+var latlong = null;
+var geoArray = [];
 
 
-if(window.location.href.indexOf("login") > -1) {
+
+
    
 
-ymaps.ready(init);
 
-function init() {
-    var geolocation = ymaps.geolocation,
-        myMap = new ymaps.Map('map', {
-            center: [55, 34],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
 
-    // Сравним положение, вычисленное по ip пользователя и
-    // положение, вычисленное средствами браузера.
-    geolocation.get({
-        provider: 'yandex',
-        mapStateAutoApply: true
-    }).then(function (result) {
-        // Красным цветом пометим положение, вычисленное через ip.
-        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
-        result.geoObjects.get(0).properties.set({
-            balloonContentBody: 'Мое местоположение'
-        });
-        myMap.geoObjects.add(result.geoObjects);
+function get_coords_by_uid(user_id){    
+    console.log("trying to get coordinates for ", user_id);
+ 
+    firebase.database().ref().child(user_id).on('value', function (snapshot) {
+        console.log(snapshot.val());
+        latlong = snapshot.val();
+
+        console.log("Здесь мы должны получить geo" , latlong);
+        geoArray = Object.values(latlong);
+        console.log(geoArray)       
+        users_coords[user_id] = snapshot.val();
+    },function(error){ 
+        console.log("Error:" +error.code)
     });
-    var location = document.getElementById('get-location');
-    location.addEventListener('click', function () {
-        geolocation.get({
-            provider: 'browser',
-            mapStateAutoApply: true
-        }).then(function (result) {
-            // Синим цветом пометим положение, полученное через браузер.
-            // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
-            result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
-            myMap.geoObjects.add(result.geoObjects);
-        });
-    });
-
-}
-window.onload = function () {
-    var geo = document.getElementById('get-current-location');
-    console.log(geo);
-    geo.addEventListener('click', function () {
-        navigator.geolocation.getCurrentPosition(function (location) {
-            var latitude = location.coords.latitude;
-            var longitude = location.coords.longitude;
-            console.log(latitude, longitude);
-            window.localStorage.setItem('location', JSON.stringify({latitude: latitude, longitude: longitude}));
+};
+var subBut = document.getElementById('submit');
+if (subBut) {
+    subBut.addEventListener('click', function(){
+        if (subBut)  {
+         console.log(subBut.value());   
+        } else {
             
-        })
+        }
     })
 }
+ 
+
+
+
+console.log (window.location.href );
+
+
+
+    // var location = document.getElementById('get-location');
+    // location.addEventListener('click', function () {
+    //     console.log("firebase", firebase);
+
+        
+    //     console.log('users_list', users_list.length);
+    //     for(var i = 0; i < users_list.length; i++) {
+    //         get_coords_by_uid(users_list[i]);
+    //     }
+    //     var getUid;
+    //     var ourUid;
+    //     setTimeout(function () {
+    //         /*Code to place coords on the map should be  */
+    //         console.log(users_coords);
+    //         for(var key in users_coords){
+    //            ourUid =  key;  
+    //         //    console.log(Object.keys(users_coords).map(key => srcObject[key].longitude)); 
+    //         }
+    //     }, 2000);
+    //     console.log(ourUid);
+       
+        
+    // });
+
+
+window.onload = function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+        var latitude = location.coords.latitude;
+        var longitude = location.coords.longitude;
+        console.log(latitude, longitude);
+        window.localStorage.setItem('location', JSON.stringify({latitude: latitude, longitude: longitude}));
+        
+    })
+
+ 
+    firebase.database().ref().child("accounts").orderByChild("userId").on("value", function(snapshot) {
+        snapshot.forEach(function(uid) {
+            var current_uid = uid.val()['userId'];
+            //var coords = get_coords_by_uid(current_uid);
+            //console.log(current_uid, coords);
+            console.log(current_uid);
+            users_list.push(current_uid)
+        });
+    });
 }
+
+
+
+console.log('куки', document.cookie);
+// if(
+//     window.location.href=="address to check"
+//     ){ymaps.ready(init);
+//         geolocation.get({
+//             provider: 'browser',
+//             mapStateAutoApply: true
+//         }).then(function (result) {
+//             // Синим цветом пометим положение, полученное через браузер.
+//             // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+//             result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+//             myMap.geoObjects.add(result.geoObjects);
+//             function init() {
+//                 var geolocation = ymaps.geolocation,
+//                     myMap = new ymaps.Map('map', {
+//                         center: [55, 34],
+//                         zoom: 10
+//                     }, {
+//                         searchControlProvider: 'yandex#search'
+//                     });
+                
+            
+            
+//                 // Сравним положение, вычисленное по ip пользователя и
+//                 // положение, вычисленное средствами браузера.
+//                 geolocation.get({
+//                     provider: 'yandex',
+//                     mapStateAutoApply: true
+//                 }).then(function (result) {
+//                     // Красным цветом пометим положение, вычисленное через ip.
+//                     result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+//                     result.geoObjects.get(0).properties.set({
+//                         balloonContentBody: 'Мое местоположение'
+//                     });
+//                     myMap.geoObjects.add(result.geoObjects);
+//                 })}
+        
--- js/controller.js
+++ js/controller.js
@@ -1,118 +1,124 @@
-let id ;	
+	
 angular.module('myApp').controller('authCtrl', 
 								  [	 
 								  	'$firebaseAuth', '$location',
 								  	iWillRuleOverTheWorld
 								  ]);
-var globalAuth;
-var userDateSaved;
-var imageUrl;
-function iWillRuleOverTheWorld ($firebaseAuth,$location) {
-	    var someOccupiedCountries = this;
+// var globalAuth;
+// var userDateSaved;
+// var imageUrl;
+// var id;
+// function iWillRuleOverTheWorld ($firebaseAuth,$location) {
+// 	    var someOccupiedCountries = this;
 	
-		var auth = $firebaseAuth();
+// 		var auth = $firebaseAuth();
 
-	someOccupiedCountries.login = function(){
+// 	someOccupiedCountries.login = function(){
 		
-		auth.$signInWithPopup('google');
+// 		auth.$signInWithPopup('google');
 	
-	};
+// 	};
 
-	someOccupiedCountries.logout = function(){
+// 	someOccupiedCountries.logout = function(){
 		
-		auth.$signOut();
+// 		auth.$signOut();
 	
-	};
+// 	};
 	
 	
-	auth.$onAuthStateChanged(function(authData ){
-	   someOccupiedCountries.author = authData;
-	//    firebase.database().ref().child('UserData').push({
-	// 	takedata: authData
+// 	auth.$onAuthStateChanged(function(authData ){
+// 	   someOccupiedCountries.author = authData;
+// 	//    firebase.database().ref().child('UserData').push({
+// 	// 	takedata: authData
 		
-	// });
-	  if(authData) {
-		globalAuth = authData;
-		userDateSaved = globalAuth.displayName;
-		imageUrl =globalAuth.photoUrl;
+// 	// });
+// 	  if(authData) {
+// 		globalAuth = authData;
+// 		userDateSaved = globalAuth.displayName;
+// 		imageUrl =globalAuth.photoUrl;
 		
-		console.log('ffff',imageUrl);
-	 	// $location.url("login");
-		// $scope.$apply(); 
-	}
-	});
+// 		console.log('ffff',imageUrl,userDateSaved);
+// 	 	// $location.url("login");
+// 		// $scope.$apply(); 
+// 	}
+// 	});
 
 	
-	};
-	myApp.controller('mainController', function ($scope) {
+// 	};
+// 	myApp.controller('mainController', function ($scope) {
 
-	})
-	myApp.controller('loginCtrl', function ($scope) {
-		console.log("ttttlenta",globalAuth);
-	});
-	myApp.controller('databaseCtrl', function ($firebaseObject) {
-		// const rootRef = firebase.database().ref().child('geodata');
-		// const ref = rootRef.child('object');
-		// this.object = $firebaseObject(ref);
-		var geoCordinates = JSON.parse(window.localStorage.getItem('location'));
-		firebase.database().ref().child(id).set({
-			latitidude: geoCordinates.latitude,
-			longitude: geoCordinates.longitude
-		});
-	});
+// 	})
+// 	myApp.controller('loginCtrl', function ($scope) {
+// 		console.log("ttttlenta",globalAuth);
+// 	});
+// 	myApp.controller('databaseCtrl', function ($firebaseObject) {
+// 		// const rootRef = firebase.database().ref().child('geodata');
+// 		// const ref = rootRef.child('object');
+// 		// this.object = $firebaseObject(ref);
+// 		var geoCordinates = JSON.parse(window.localStorage.getItem('location'));
+// 		firebase.database().ref().child(id).set({
+// 			latitidude: geoCordinates.latitude,
+// 			longitude: geoCordinates.longitude
+// 		});
+// 	});
 
 
-firebase.auth().onAuthStateChanged((user) => {
-	if (user) {
-	  console.log('rrr',user.uid);
-	  id = user.uid;
-	  console.log(1111,id);
-
+// firebase.auth().onAuthStateChanged((user) => {
+// 	if (user) {
+// 	  console.log('rrr',user.uid);
+// 	  id = user.uid;
+// 	  console.log(1111,id);
+// 	  document.cookie = id;
+// 	//   console.log("cccc", document.cookie);
 	
-	  firebase.database().ref().child('accounts').orderByChild("userId").equalTo(id).once("value", function (snapshot) {
-		var alreadyExists = false; 
-		snapshot.forEach(function (child) {
-			alreadyExists = true;
-		  });
-		if(!alreadyExists) {
-			console.log("Creating new user ");
-			firebase.database().ref().child('accounts').push({
-				userId: id
-		   });
-		}  
-	  })
+// 	  firebase.database().ref().child('accounts').orderByChild("userId").equalTo(id).once("value", function (snapshot) {
+// 		var alreadyExists = false; 
+// 		snapshot.forEach(function (child) {
+// 			alreadyExists = true;
+// 		  });
+// 		if(!alreadyExists) {
+// 			console.log("Creating new user ");
+// 			firebase.database().ref().child('accounts').push({
+// 				userId: id
+// 		   });
+// 		}  
+// 	  })
 
 
-	}
-  });
-//   firebase.database().ref().child('accounts').push({
-	
-// 	userId: id
-//   })
-document.addEventListener('DOMContentLoaded', function () {console.log(1111,id);
+// 	}
+//   });
+// //   firebase.database().ref().child('accounts').push({
  
-});
- function valid (form,$location){
-var name = form.textarea.value;
+// // 	userId: id
+// //   })
+// document.addEventListener('DOMContentLoaded', function () {console.log(1111,id);
+ 
+// });
+//  function valid (form,$location){
+// var name = form.textarea.value;
 
 
 
-// for(var key in form.selectChoice) {
-// 	console.log(key);
-// 	console.log(form.selectChoice[key]);
-// }
+// // for(var key in form.selectChoice) {
+// // 	console.log(key);
+// // 	console.log(form.selectChoice[key]);
+// // }
 
-var index = form.selectChoice.options.selectedIndex;
-var selection = form.selectChoice.options[index].innerText;
+// var index = form.selectChoice.options.selectedIndex;
+// var selection = form.selectChoice.options[index].innerText;
 
 
+// document.getElementById('submit').addEventListener('click', function(e){
+// 	console.log(1);
+// 	$location.url("login");
+// 	$scope.$apply(); 
+// });
 
-document.getElementById('submit').addEventListener('click', function(e){
-	console.log(1);
-	$location.url("login");
-	$scope.$apply(); 
-});
+// document.getElementById('submit').addEventListener('click', function(e){
+// 	console.log(1);
+// 	$location.url("login");
+// 	$scope.$apply(); 
+// });
 
-};
-
+// };
 
